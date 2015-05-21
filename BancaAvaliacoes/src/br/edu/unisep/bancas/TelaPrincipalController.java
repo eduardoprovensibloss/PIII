f2f2a4230bce1b35891262c303b695daf4ca204b
rcpadilha <roberto@Robertos-MBP.home>
2015-05-21 15:52:36 -0300

    Importação Inicial

--- BancaAvaliacoes/src/br/edu/unisep/bancas/TelaPrincipalController.java
+++ BancaAvaliacoes/src/br/edu/unisep/bancas/TelaPrincipalController.java
@@ -1,13 +1,6 @@
 package br.edu.unisep.bancas;
 
-import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
-
 public class TelaPrincipalController {
 
-	@FXML
-	public void clicou(MouseEvent e) {
-		System.out.println("Clicou");
-	}
-	
+
 }
